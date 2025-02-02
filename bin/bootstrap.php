4ca735a8ec9d0a4baff03ca30ebd72b6211ed605
Mistralys <s.mordziol@gmail.com>
2025-02-02 22:08:51 +0100

    Added some constants.

--- bin/bootstrap.php
+++ bin/bootstrap.php
@@ -14,3 +14,6 @@ if(!file_exists(__DIR__.'/../config.php')) {
 
 require_once __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/../config.php';
+
+const BAT_SUBFOLDER_NAME = 'bat';
+const CONSOLE_OUTPUT_LOG_PATH = 'Data/SFSE/Plugins/sfse_plugin_console.log';
